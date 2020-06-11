f50b278ee9072d2b0a769824fb0826ba96af4f11
Robert Groves <rwgdev@gmail.com>
2020-06-10 19:38:19 -0500

    Create puzzles.js and include in main html.

---
+++ js/puzzles.js
@@ -0,0 +1,5 @@
+const puzzles = [
+  { category: "Website", text: "freecodecamp.org" },
+  { category: "Thing", text: "World Wide Web" },
+  { cateogry: "Test", text: "Test: Puzzle-text, having punctuation; Great!" },
+];
